c3ff5f8e5ef3dcf45691c0fae465f187ae907652
subuta <shunta.3110@gmail.com>
2018-11-15 22:56:20 +0900

    Comment out wdyu.

--- docs/index.js
+++ docs/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom'
 
 import App from './components/App'
 
-if (process.env.NODE_ENV !== 'production') {
-  const {whyDidYouUpdate} = require('why-did-you-update');
-  whyDidYouUpdate(React);
-}
+// Only for debug.
+// if (process.env.NODE_ENV !== 'production') {
+//   const {whyDidYouUpdate} = require('why-did-you-update');
+//   whyDidYouUpdate(React);
+// }
 
 ReactDOM.render(<App />, document.querySelector('#app'))
